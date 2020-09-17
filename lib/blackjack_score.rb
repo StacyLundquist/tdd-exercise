7f60952a19933a2e0ca78a11c92f12223d3f791b
Stacy <lundquist.stacy@gmail.com>
2020-09-17 09:36:11 -0700

    So far...

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,5 +3,15 @@
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
+  score = 0
+  possible_hand = hand.split("")
+
+  possible_hand.each do |num|
+    case hand_score_numbers
+    when [\d].match(num)
+    score += num
+    end
+    when
+  end
 
 end
