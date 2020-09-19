c3a28f3a35012ae5248dd6f8fde1b90cd2cfad29
Stacy <lundquist.stacy@gmail.com>
2020-09-18 19:32:49 -0700

    A couple more hours on BlackJack for Stacy

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -4,13 +4,27 @@ VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
   score = 0
-  possible_hand = hand.split("")
+  aces_in_hand = 0
 
-  possible_hand.each do |num|
-    case hand_score_numbers
-    when [\d].match(num)
-    score += num
+  hand.each do |num|
+    if num.instance_of?(Integer)
+      score += num
+    else
+      score += 10
+    end
+    if num == "Ace"
+      score += 1
+      aces_in_hand += 1
+    end
+    if num != VALID_CARDS
+      raise ArgumentError.new("That's not a card")
     end
   end
 
+  if score > 21 && aces_in_hand > 0
+    score -= 10
+  end
+
+
+  return score
 end
